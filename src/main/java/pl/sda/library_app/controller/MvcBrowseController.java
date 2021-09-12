d3063ca2e31b7743fdaddc96207083f0df33a01e
Adam Dyczkowski <atomdyczkowski@gmail.com>
2021-09-12 13:33:49 +0200

    added order button functionality

--- src/main/java/pl/sda/library_app/controller/MvcBrowseController.java
+++ src/main/java/pl/sda/library_app/controller/MvcBrowseController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.sda.library_app.models.Book;
 import pl.sda.library_app.services.BooksService;
@@ -20,6 +22,12 @@ public class MvcBrowseController {
     public String getAllBooks(Model model) {
         List<Book> books = booksService.getAll();
         model.addAttribute("books", books);
+        return "catalog";
+    }
+
+    @PostMapping("/{id}/order")
+    public String orderBook(@PathVariable Long id) {
+        System.out.println("Ordered book " + id);
         return "catalog";
     }
 }
