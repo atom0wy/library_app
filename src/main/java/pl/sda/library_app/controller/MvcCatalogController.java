01287d5b1098a0ded73e7225f40c4671a60883b1
Adam Dyczkowski <atomdyczkowski@gmail.com>
2021-09-12 21:12:27 +0200

    catalog controller changes and new ordering method

--- src/main/java/pl/sda/library_app/controller/MvcCatalogController.java
+++ src/main/java/pl/sda/library_app/controller/MvcCatalogController.java
@@ -9,14 +9,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.sda.library_app.models.Book;
 import pl.sda.library_app.services.BooksService;
+import pl.sda.library_app.services.OrderService;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
 @RequestMapping("/catalog")
 @RequiredArgsConstructor
-public class MvcBrowseController {
+public class MvcCatalogController {
     private final BooksService booksService;
+    private final OrderService orderService;
 
     @GetMapping
     public String getAllBooks(Model model) {
@@ -26,8 +29,11 @@ public class MvcBrowseController {
     }
 
     @PostMapping("/{id}/order")
-    public String orderBook(@PathVariable Long id) {
+    public String orderBook(@PathVariable Long id, Principal principal, Model model) {
         System.out.println("Ordered book " + id);
+        orderService.makeOrder(id, principal.getName());
+        List<Book> books = booksService.getAll();
+        model.addAttribute("books", books);
         return "catalog";
     }
 }
